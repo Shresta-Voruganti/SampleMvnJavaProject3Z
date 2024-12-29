7ed48b7f1888dfbb7d653cd9e328ec6978355385
Shresta-Voruganti <22bd1a053z@gmail.com>
2024-12-29 18:12:03 +0530

    sammple maven java project

---
+++ src/main/java/com/app/demo/SampleMvnJavaProj/App.java
@@ -0,0 +1,13 @@
+package com.app.demo.SampleMvnJavaProj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "New Maven Project53Z" );
+    }
+}
